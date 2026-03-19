bbc58211b64065a417aed76e90cb773deb98eb40
Carol Kithinji <carolkithinji35@gmail.com>
2026-03-19 23:48:27 +0300

    add correct module exports

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -14,12 +14,10 @@ function removeLastProduct() {
 
 // Export the necessary parts for testing
 module.exports = {
-  logFirstProduct:
-    typeof logFirstProduct !== "undefined" ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== "undefined" ? addProduct : undefined,
-  updateProductName:
-    typeof updateProductName !== "undefined" ? updateProductName : undefined,
-  removeLastProduct:
-    typeof removeLastProduct !== "undefined" ? removeLastProduct : undefined,
   products,
+  logFirstProduct,
+  addProduct,
+  updateProductName,
+  removeLastProduct,
 };
+console.log()
