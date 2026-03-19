4a813292cd22412d23587082d3a6e0f3a110327c
Carol Kithinji <carolkithinji35@gmail.com>
2026-03-19 23:38:55 +0300

    Initial commit

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,12 +1,25 @@
-// Write your code here
-
-
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
+function logFirstProduct() {
+  console.log(products[0]);
+}
+function addProduct(product) {
+  return products.push(product);
+}
+function updateProductName(index, newName) {
+  return products[index] = newName;
+}
+function removeLastProduct() {
+  return products.pop();
+}
 
 // Export the necessary parts for testing
 module.exports = {
-  logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
-  addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
-  updateProductName: typeof updateProductName !== 'undefined' ? updateProductName : undefined,
-  removeLastProduct: typeof removeLastProduct !== 'undefined' ? removeLastProduct : undefined,
-  products
+  logFirstProduct:
+    typeof logFirstProduct !== "undefined" ? logFirstProduct : undefined,
+  addProduct: typeof addProduct !== "undefined" ? addProduct : undefined,
+  updateProductName:
+    typeof updateProductName !== "undefined" ? updateProductName : undefined,
+  removeLastProduct:
+    typeof removeLastProduct !== "undefined" ? removeLastProduct : undefined,
+  products,
 };
